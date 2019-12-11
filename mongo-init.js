c4b06e3c84dec71544bd1cddad409a7099059c84
David Garcia <david.garcianunez@telefonica.com>
2019-12-11 18:28:45 +0100

    push first and default user to mongodb init script

---
+++ mongo-init.js
@@ -0,0 +1,9 @@
+db.users.update(
+  { username: "admin" },
+  {
+    username: "admin",
+    password:
+      "$6$rounds=656000$X7b.3H5FnxG.H1mZ$onE/9/Nj91AMQx7kGesLBY17LX5my186ggcGg7RNWRZWEuwehAsrckcfD40179NyqtOFe8f5U/Uja98Ji0Be.0"
+  },
+  { upsert: true }
+);
