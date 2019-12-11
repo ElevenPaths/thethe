01e6afe040a11af64503aab84a134d02b724d7fd
David Garcia <david.garcianunez@telefonica.com>
2019-12-11 16:18:14 +0100

    fix restoration file

--- utils/restore_thethe_db.sh
+++ utils/restore_thethe_db.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # https://jeromejaglale.com/doc/programming/mongodb_docker_mongodump_mongorestore
 
-gzip -d db.dump.gz
+gzip -kd db.dump.gz
 docker exec -i thethe_mongo_1 sh -c 'mongorestore --username root  --password root --authenticationDatabase admin --archive' < db.dump
+rm db.dump
