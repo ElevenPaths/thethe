28296b7becb4fe53da7865d4296085a2414fc01a
David Garcia <david.garcianunez@telefonica.com>
2020-01-22 15:26:59 +0100

    fix filter by extension

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -6,14 +6,11 @@ from celery import Celery
 EXCLUDE_SET = ["plugins.py", "__init__.py", "plugin_base.py", "TEMPLATE.py"]
 
 plugins = []
-for root, dirs, files in os.walk('server/plugins'):
+for root, dirs, files in os.walk("server/plugins"):
     for file in files:
-        if '.py' in file and not file in EXCLUDE_SET:
+        if ".py" in file[-3:] and not file in EXCLUDE_SET:
             plugins.append("server.plugins.{}".format(os.path.splitext(file)[0]))
 
 celery_app = Celery(
-    "tasks",
-    backend="redis://redis",
-    broker="redis://redis:6379/0",
-    include=plugins
+    "tasks", backend="redis://redis", broker="redis://redis:6379/0", include=plugins,
 )
