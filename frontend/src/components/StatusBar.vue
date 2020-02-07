f442221f785b9066a9f80f1e9397140d5c57f461
David Garcia <david.garcianunez@telefonica.com>
2020-02-07 11:05:54 +0100

    change font weight

--- frontend/src/components/StatusBar.vue
+++ frontend/src/components/StatusBar.vue
@@ -1,17 +1,11 @@
 <template>
   <v-footer absolute class="grey darken-3">
-    <span lg1 class="text-md-center white--text px-2">v{{ version }}</span>
+    <span lg1 class="text-md-center white--text px-2 font-weight-bold">v{{ version }}</span>
     <v-spacer></v-spacer>
     <v-btn flat href="https://elevenpaths.com" target="_blank">
-      <v-img
-        :src="require('@/assets/11p_logo.png')"
-        contain
-        max-width="64"
-      ></v-img>
+      <v-img :src="require('@/assets/11p_logo.png')" contain max-width="64"></v-img>
     </v-btn>
-    <span lg1 class="text-md-center white--text px-2"
-      >&copy;&nbsp;{{ new Date().getFullYear() }}</span
-    >
+    <span lg1 class="text-md-center white--text px-2">&copy;&nbsp;{{ new Date().getFullYear() }}</span>
   </v-footer>
 </template>
 
