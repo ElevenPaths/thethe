d4b7acb0e9d3b438051fa13a5b985bf603c4d806
David Garcia <david.garcianunez@telefonica.com>
2020-02-14 21:55:31 +0100

    update backup scripts. No longer necessary to supply the container name

--- utils/backup_thethe_db.sh
+++ utils/backup_thethe_db.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # https://jeromejaglale.com/doc/programming/mongodb_docker_mongodump_mongorestore
 
-if [ "$1" == "" ]; then
-    echo "Usage: $0 <name_of_mongodb_container>"
-    exit 1
-fi
-
-docker exec $1 sh -c 'mongodump --username root  --password root --authenticationDatabase admin --archive' > db.dump
+echo "Doing database backup..."
+docker exec thethe_mongo sh -c 'mongodump --username root  --password root --authenticationDatabase admin --archive' > db.dump
 gzip -c db.dump > db.dump.gz
 rm db.dump
--- utils/restore_thethe_db.sh
+++ utils/restore_thethe_db.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # https://jeromejaglale.com/doc/programming/mongodb_docker_mongodump_mongorestore
 
-if [ "$1" == "" ]; then
-    echo "Usage: $0 <name_of_mongodb_container>"
-    exit 1
-fi
-
+echo "Restoring database from backup..."
 gzip -kd db.dump.gz
-docker exec -i $1 sh -c 'mongorestore --drop --username root  --password root --authenticationDatabase admin --archive' < db.dump
+docker exec -i thethe_mongo sh -c 'mongorestore --drop --username root  --password root --authenticationDatabase admin --archive' < db.dump
 rm db.dump
