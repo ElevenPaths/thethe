8ece9e3bb35c538a177185682d382bdbb412826b
David Garcia <david.garcianunez@telefonica.com>
2020-02-26 09:48:12 +0100

    added project_refs upon user creation

--- mongo-init.js
+++ mongo-init.js
@@ -3,7 +3,8 @@ db.users.update(
   {
     username: "admin",
     password:
-      "$6$rounds=656000$X7b.3H5FnxG.H1mZ$onE/9/Nj91AMQx7kGesLBY17LX5my186ggcGg7RNWRZWEuwehAsrckcfD40179NyqtOFe8f5U/Uja98Ji0Be.0"
+      "$6$rounds=656000$X7b.3H5FnxG.H1mZ$onE/9/Nj91AMQx7kGesLBY17LX5my186ggcGg7RNWRZWEuwehAsrckcfD40179NyqtOFe8f5U/Uja98Ji0Be.0",
+    project_refs: []
   },
   { upsert: true }
 );
