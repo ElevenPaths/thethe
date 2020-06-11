ce276d288358d5809f4ab5893a08f53ce46e98f3
David Garcia <david.garcianunez@telefonica.com>
2020-06-11 20:21:53 +0200

    Modified db utils scripts

--- utils/restore_thethe_db.sh
+++ utils/restore_thethe_db.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # https://jeromejaglale.com/doc/programming/mongodb_docker_mongodump_mongorestore
+set -e
 
+echo "usage: ./restore_thethe_db.sh <mongo_user> <mongo_password>"
 echo "Restoring database from backup..."
 gzip -kd db.dump.gz
-docker exec -i thethe_mongo sh -c 'mongorestore --drop --username root  --password root --authenticationDatabase admin --archive' < db.dump
+docker exec -i thethe_mongo sh -c "mongorestore --drop --username $1  --password $2 --authenticationDatabase admin --archive" <db.dump
 rm db.dump
